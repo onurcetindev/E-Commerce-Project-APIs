465cd7c76387b1e149b72fce6ceda77eab0ddc9f
Onur Cetin <onucetin003@gmail.com>
2024-07-15 19:06:26 +0300

    ResponseEntity Implemented

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -3,7 +3,11 @@ package com.ecommerce.project.controller;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,11 +34,15 @@ public class CategoryController {
     }
 
     @DeleteMapping("/api/admin/categories/{categoryId}")
-    public String deleteCategory(@PathVariable Long categoryId) {
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
         //Bir HTTP isteği sırasında URI'nin belirli bir bölümünden veri alınmasını sağlar.
-        String status = categoryService.deleteCategory(categoryId);
+        try {
+            String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        }catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
 
-        return status;
+        }
     }
 
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,11 +27,11 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public String  deleteCategory(Long categoryId) {
         Category category = categories.stream().filter(c->c.getCategoryId().equals(categoryId))
-                .findFirst().orElse(null);
+                .findFirst().
+                orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource Not Found"));
+        //Stream API'si, koleksiyonlar (List, Set, Map gibi) veya diziler üzerinde ardışık işlemler
+        // yapmayı sağlayan bir yapıdır
 
-        if (category == null) {
-            return "Category not found";
-        }
         categories.remove(category);
         return "Category with categoryId: " + categoryId + " deleted";
 
