6e62fa6758a507cb1b6261d29ba7f8ed3298dada
Onur Cetin <onucetin003@gmail.com>
2024-07-15 14:52:14 +0300

    I just added if check for delete category id

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -25,7 +25,11 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public String  deleteCategory(Long categoryId) {
         Category category = categories.stream().filter(c->c.getCategoryId().equals(categoryId))
-                .findFirst().get();
+                .findFirst().orElse(null);
+
+        if (category == null) {
+            return "Category not found";
+        }
         categories.remove(category);
         return "Category with categoryId: " + categoryId + " deleted";
 
