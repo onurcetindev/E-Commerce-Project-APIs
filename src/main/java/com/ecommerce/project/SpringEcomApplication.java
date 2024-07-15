bc45ecf956aff288d0be7195923a326696541ec4
Onur Cetin <onucetin003@gmail.com>
2024-07-15 12:58:05 +0300

    Get All Categories added

--- src/main/java/com/ecommerce/project/SpringEcomApplication.java
+++ src/main/java/com/ecommerce/project/SpringEcomApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringEcomApplication {
+public class  SpringEcomApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEcomApplication.class, args);
