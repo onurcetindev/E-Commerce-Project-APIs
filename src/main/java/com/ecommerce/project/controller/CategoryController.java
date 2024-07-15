de720450d8d7bfb00662ff52b1fc2b38612ad6d6
Onur Cetin <onucetin003@gmail.com>
2024-07-15 14:23:59 +0300

    Setting Category Service and Managing IDs

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,23 +11,28 @@ import java.util.List;
 @RestController
 public class CategoryController {
 
-    private List<Category> categories = new ArrayList<>();
+    @Autowired
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
     @GetMapping("/api/public/categories")
     public List<Category> getCategories() {
-        return categories;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/public/categories")
     public String createCategory(@RequestBody Category category) {
-        categories.add(category);
+        categoryService.createCategory(category);
         return "Category added successfully";
     }
 
-    @DeleteMapping("/api/public/categories")
+   /* @DeleteMapping("/api/public/categories")
     public String deleteCategory(@RequestBody Category category) {
-        categories.remove(category);
+        categoryService.deleteCategory(category);
         return "Category deleted successfully";
-    }
+    } */
 
 }
