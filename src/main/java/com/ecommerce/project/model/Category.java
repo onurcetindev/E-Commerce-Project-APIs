25f3aaefda34c00ea68d6b9498dd92cc5495845e
Onur Cetin <onucetin003@gmail.com>
2024-07-21 22:02:49 +0300

    Custom Error Handling Mechanism. If an empty value is entered in the category name, we opened an ExceptionHandler class for it and printed on the screen as user friendly, empty value cannot be entered.

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity(name = "categories")
@@ -13,13 +14,13 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 
-
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
     @NotBlank
+    @Size(min = 3, message = "Category name must contain at least 3 character")
     private String categoryName;
 
 }
