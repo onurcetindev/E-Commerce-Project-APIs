ba35313aeb1f28715b3258eaba956f6ae3d3521f
Onur Cetin <onucetin003@gmail.com>
2024-07-21 14:42:56 +0300

    Making use of Hibernate Validator

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity(name = "categories")
@@ -17,6 +18,8 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+
+    @NotBlank
     private String categoryName;
 
 }
