ab08feeead528f51f4eae6bd8ff06918a21ede58
Onur Cetin <onucetin003@gmail.com>
2024-07-22 14:54:19 +0300

    Customer Exceptions | APIException added

--- src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.ecommerce.project.exceptions;
 
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,5 +27,17 @@ public class MyGlobalExceptionHandler {
             response.put(fieldName,message);
         });
         return new ResponseEntity<Map<String ,String>>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e){
+        String message = e.getMessage();
+        return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<String> myAPIException(APIException e){
+        String message = e.getMessage();
+        return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -13,4 +13,5 @@ public interface CategoryService {
     void createCategory(Category category);
     String  deleteCategory(Long categoryId);
     Category updateCategory(Category category, Long categoryId);
+
 }
