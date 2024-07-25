b462e04dee6f181815bc6831b03fc7842bf53131
Onur Cetin <onucetin003@gmail.com>
2024-07-25 15:21:53 +0300

    Accepting Pagination Parameters

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -33,9 +33,14 @@ public class CategoryController {
 
 
     @GetMapping("/public/categories")
-    public ResponseEntity<CategoryResponse> getCategories() {
-        CategoryResponse categoryResponse = categoryService.getAllCategories();
+    public ResponseEntity<CategoryResponse> getCategories(
+            @RequestParam(name = "pageNumber") Integer pageNumber,
+            @RequestParam(name = "pageSize") Integer pageSize)
+    {
+
+        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber,pageSize);
         return  new ResponseEntity<>(categoryResponse,HttpStatus.OK);
+
     }
 
     @PostMapping("/public/categories")
