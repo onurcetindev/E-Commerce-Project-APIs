f257efd101ad9b4b5a02f223da3b61f970289096
Onur Cetin <onucetin003@gmail.com>
2024-07-25 18:03:32 +0300

    Adding default values for if user sende without page parameters

---
+++ src/main/java/com/ecommerce/project/config/AppConstants.java
@@ -0,0 +1,10 @@
+package com.ecommerce.project.config;
+
+public class AppConstants {
+
+    //for http://localhost:8081/api/public/categories
+    public static final String PAGE_NUMBER = "0";
+    public static final String PAGE_SIZE = "50";
+
+
+}
