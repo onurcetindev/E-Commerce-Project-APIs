472dd7713557e4cb8b3e6497b359001401271c88
Onur Cetin <onucetin003@gmail.com>
2024-08-01 21:30:29 +0300

    Configuring Project to work with JWT

---
+++ src/main/java/com/ecommerce/project/security/jwt/AuthEntryPointJwt.java
@@ -0,0 +1,45 @@
+package com.ecommerce.project.security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+//JWT kimlik doğrulama sürecinde yetkisiz erişim durumlarında özel bir hata yanıtı dönen bir
+// AuthenticationEntryPoint olan AuthEntryPointJwt sınıfını tanımlar. Spring Security'de AuthenticationEntryPoint,
+// kimlik doğrulama hatası meydana geldiğinde nasıl bir yanıt döneceğini belirler.
+@Component
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException, ServletException {
+        logger.error("Unauthorized error: {}", authException.getMessage());
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        final Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        body.put("error", "Unauthorized");
+        body.put("message", authException.getMessage());
+        body.put("path", request.getServletPath());
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
+    }
+
+}
